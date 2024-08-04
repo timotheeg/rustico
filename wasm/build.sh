740f05e358181c8d5bb4f40a437ea8f8e0c6b26f
Timothee Groleau <timothee@open.gov.sg>
2024-08-04 17:46:54 +0800

    chore(build): specify nightly build in local toolchain file

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p ./public
-cargo +nightly build --target wasm32-unknown-unknown --release
+cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen target/wasm32-unknown-unknown/release/rustico_wasm.wasm --out-dir ./public --no-modules
